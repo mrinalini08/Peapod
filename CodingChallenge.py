578d2e6e660f1ee97e651f7a03a98e0499c9a13a
Russel <russ@Russels-iMac.local>
2022-02-16 14:01:07 -0500

    coding screen

---
+++ CodingChallenge.py
@@ -0,0 +1,44 @@
+""" WELCOME TO THE PDL CODING CHALLENGE!! """
+
+'''
+STEP 1:
+
+Read both data files: transaction_information.csv and customer_preferences.json
+'''
+
+
+
+'''
+STEP 2: 
+
+Merge the dataframes in to one master dataframe
+
+There are 500 customers in the transaction dataframe and 300 customers in the customer preference dataframe
+Only keep records which appear in both
+
+remove the objects in memory that are no longer needed
+'''
+
+
+
+
+''' 
+STEP 3:
+
+Calculate total sales by month for the months of Jan and Feb
+
+'''
+
+
+
+'''
+STEP 4:
+
+Use any model of your choice to understand the relationship between a person's sales and other metrics
+
+Make sure to use categorical and continuous variables
+'''
+
+
+
+
